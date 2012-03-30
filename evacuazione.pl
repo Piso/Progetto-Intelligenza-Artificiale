d8697a9488bd30e4c05c4d844090388604e0790c
Andrea Piso PC Fisso <andreapiso@gmail.com>
2012-03-30 21:58:57 +0200

    Aggiunta trovato e altro

--- evacuazione.pl
+++ evacuazione.pl
@@ -16,6 +16,8 @@ type strada --> st(incrocio,incrocio,int).
 
 %il tipo stato definisce la posizione sugli incroci di OGNI gruppo
 %provvisorio: solo un gruppo!!
+%FIXME: bisogna inserire un' info anche riguardante il numero
+%del "turno di movimento"? forse serve x costo!!
 type stato -->in(incrocio).
 
 pred sicuro(incrocio).
@@ -24,3 +26,14 @@ pred agibile(strada).
 
 %il predicato trafficata non so se sia necessario
 pred trafficata(strada).
+
+
+pred trovato(stato).
+
+pred mossa(stato,stato,strada).
+
+trovato(in(Inc1)):-sicuro(Inc1).
+
+%Mossa non so se e' corretto (controllare unificazione road=
+%sicuramente e' incompleto perchè mancano tutte le info relative a costo
+mossa(in(Inc1),in(Inc2),Road):- Road=st(Inc1,Inc2,_),agibile(Road).
