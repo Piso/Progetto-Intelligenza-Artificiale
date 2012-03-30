bdab552214ebd369b9e8dd79643fd5cf348fd2be
Andrea Piso PC Fisso <andreapiso@gmail.com>
2012-03-30 20:11:43 +0200

    Modifica evacuazione.pl

--- evacuazione.pl
+++ evacuazione.pl
@@ -2,7 +2,25 @@
 %
 :- use_module('types/chk').
 
-type nodo --> .
-type stato --> .
-type list(El) -->.
+%classico tipo liste
+type list(El) -->[];[El|list(El)].
 
+%per ora ho pensato di mettere solo i nomi incrocio delegando a strada
+%i collegamenti tra incroci e i pesi
+%per ora ho messo solo 6 incroci. Aumentabile a piacere.
+
+type incrocio -->incrocio1;incrocio2;incrocio3;incrocio4;incrocio5;incrocio6.
+
+%tipo strada->st(incrocio,incrocio collegato,peso della strada)
+type strada --> st(incrocio,incrocio,int).
+
+%il tipo stato definisce la posizione sugli incroci di OGNI gruppo
+%provvisorio: solo un gruppo!!
+type stato -->in(incrocio).
+
+pred sicuro(incrocio).
+
+pred agibile(strada).
+
+%il predicato trafficata non so se sia necessario
+pred trafficata(strada).
