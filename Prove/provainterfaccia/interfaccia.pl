0d7fcca5afb8648f569ca46943f3fb30f0152fe6
luca palmulli <luca.palmulli@gmail.com>
2012-04-02 18:50:57 +0200

    Interfaccia Work in progress

--- Prove/provainterfaccia/interfaccia.pl
+++ Prove/provainterfaccia/interfaccia.pl
@@ -1,22 +1,32 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % TEST INTERFACCIA %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%
+:-dynamic(showflag/0).
+
+
+:-writeln('Inizio Programma. Caricamento esecuzione interfaccia.').
+:-writeln('Comandi:  load_problem(+F:nome_file), solve, show, noshow\n\n').
+
+solve(_,Y):-trovato(Y).
 
 load_problem(F) :-consult(F).
 show :-	writeln('Esecuzione in Corso.'),
 	listaComandi.
 listaComandi:-writeln('Comandi: r: solve(X,Y); h:help').
 
+
 comandi :- readln(F),
 	(
 	F = [], !;
-	F = [r|_],!,
+	F = [r|_],
 	    write('Inserisci stato di partenza gruppo1: '),
 	    readln(X),
 	    write('Inserisci stato di partenza gruppo2: '),
-	    readln(Y),solve(in(X,Y),Z),
+	    readln(Y),solve(in(X,Y),_),
 	    write('. Computazione Terminata.');
-	F = [h|_],!,listacomandi,command;
+	F = [h|_],!,listaComandi,comandi;
 	F = [a|_],!,abort;
 	true
 	).
